5f5fa4a48a142f604dd8cd115e570207d6e8a109
seth-pogi <johnericgaviola4@gmail.com>
2025-07-15 10:59:00 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,34 +1,4 @@
 return {
-  --Seth Main
-  [7262715069] = true,
-  --Seth Alt 1
-  [7488852218] = true,
-  --Seth Alt 2
-  [8715739547] = true,
-  --Renn Main
-  [7404802184] = true, 
-  --Renn Alt
-  [8682438191] = true,
-  --Kevvv Main
-  [3380703051] = true,
-  --Selur Main
-  [1240860265] = true,
-  --Vinny Main
-  [6081624822] = true,
-  -- Paw Main
-  [5057536137] = true,
-  -- Yams Main
-  [3219789093] = true,
-  -- Mark Main
-  [5764721857] = true,
-  -- Jay Main
-  [3034004536] = true, 
-  -- Sudeis acc 1
-  [2289904334] = true,
-  -- Sudeis acc 2
-  [4346640880] = true,
-  -- Sudeis acc 3
-  [3035004615] = true,
-  -- Sudeis acc 4
-  [3053406370] = true,
-} 
+    ["BE_Y0URSS"] = 123456789,
+    ["TPX_AltJohn1"] = 987654321
+}
